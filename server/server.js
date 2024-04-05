88d31402f01d8cc460fcb2cbe6651a277c9c04b1
Anavi-K <anavikothari@outlook.com>
2024-04-05 16:51:02 +0530

    added mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PUBLIC_PORT || 3000;
+const port = 3000;
+const mongoose = require('mongoose');
+const mongodburi = process.env.MONGO_DB;
 
 app.get('/ping', (req, res) => {
     res.send('pong');
+  });
+
+  //connecting mongodb
+  mongoose.connect(mongodburi)
+
+  //define a route to check MongoDB connetions status
+  app.get('/mongodbconnection', (req, res) => {
+    if(mongoose.connection.readyState === 1){
+      res.status(200).json('MongoDB is connected successfully')
+    }else{
+      res.status(400).json('Error connecting to MongoDB')
+    }
   });
   
   if (require.main === module) {
